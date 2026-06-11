b9999f9d2319f15f6fe7ffc35d806be32a4edd18
Crawfish-h <jaidendcrawford@gmail.com>
2026-06-11 08:45:00 -0400

    Added more functionality to the Mdyn.New function.

--- Mdyn.cs
+++ Mdyn.cs
@@ -1,21 +1,57 @@
 namespace MDyn
 {
-    struct MDyn
+    struct Mdyn
     {
+        enum Value_Type
+        {
+            Int,
+            Doublev,
+            Bool,
+            Object,
+        }
+
+        Value_Type Vtype;
         int? Int;
         double? Doublev;
         bool? Bool;
         object? Object;
-        List<int>? List_Int;
-        List<double>? List;
-        List<bool>? List;
-        List<int>? List;
+        Dictionary<string, System.Reflection.PropertyInfo>? Members;
 
-        public static MDyn New<T>(T value)
+        public static Mdyn New<T>(T value)
         {
-            if (value is int) 
+            Mdyn mdyn = new();
+            if (value is int)
+            {
+                mdyn.Int = value as int?;
+                mdyn.Vtype = Value_Type.Int;
+                Console.WriteLine("Int");
+            }else if (value is double)
+            {
+                mdyn.Doublev = value as double?;
+                mdyn.Vtype = Value_Type.Doublev;
+                Console.WriteLine("Doublev");
+            }else if (value is bool)
+            {
+                mdyn.Bool = value as bool?;
+                mdyn.Vtype = Value_Type.Bool;
+                Console.WriteLine("Bool");
+            }else if (value is not decimal and not float and not null && value is not byte)
+            {
+                mdyn.Object = value;
+                mdyn.Vtype = Value_Type.Object;
+                Console.WriteLine("Object");
 
-            return new();
+                //foreach (var item in value.GetType().GetProperties())
+                //    item.SetValue(value, 20);
+                
+                foreach (var item in value.GetType().GetProperties()) {
+                    mdyn.Members?.Add(item.Name, item);
+                }
+            }
+
+            return mdyn;
         }
+
+        
     }
 }
